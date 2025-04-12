6df12efe8423b9fbad5684c9bf11cc1438c7f537
Dr. Subir Sarkar <dr.subirsarkar46@gmail.com>
2025-04-12 10:31:24 +0530

    populating the git page.

---
+++ docs/source/conf.py
@@ -0,0 +1,50 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '2nd Sem Skill Enhancement of Physics'
+copyright = '2025, Dr. Subir Sarkar'
+author = 'Dr. Subir Sarkar'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import sphinx_rtd_theme
+
+extensions = [
+    'sphinx_rtd_theme',
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.autodoc',
+    'sphinx_sitemap', 
+]
+
+html_theme = 'sphinx_rtd_theme'
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme' 
+html_static_path = ['_static']
+
+
+# Optional: suppress warnings about Jupyter kernel metadata
+nbsphinx_allow_errors = True
+
+
+# The full base URL for your GitHub Pages site (important!)
+html_baseurl = 'https://SubirSarkar2021.github.io/4SemCoreLabManual/'
+
+# Optional: Make sure trailing slashes match your URLs
+sitemap_url_scheme = "{link}"
---
+++ docs/source/index.rst
@@ -0,0 +1,29 @@
+.. 2nd Sem Skill Enhancement Course documentation master file, created by
+   sphinx-quickstart on Fri Apr 11 22:40:42 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+2nd Sem Skill Enhancement Course documentation
+==============================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   Notebook/1CheckPrime
+   Notebook/2Fibonacci
+   Notebook/3SumMatrices
+   Notebook/5SumSeries
+   Notebook/6Sine Series
+   Notebook/7TransformationCoordinate
+   Notebook/8Projectile
+   Notebook/9Plot
+   Notebook/10_Lissajous
+
+   
+
